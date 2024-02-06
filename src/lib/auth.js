66ce31de26e7f73cf07e4c3efc9faf921643e1cf
Your Name <you@example.com>
2024-02-06 16:55:53 +0100

    restrict access to all pages unless login

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -4,7 +4,7 @@ import { connectToDb } from "./utils";
 import { User } from "./models";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
-// import { authConfig } from "./auth.config";
+import { authConfig } from "./auth.config";
 
 const login = async (credentials) => {
   try {
@@ -33,7 +33,7 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
-  //   ...authConfig,
+  ...authConfig,
   providers: [
     GitHub({
       clientId: process.env.GITHUB_ID,
@@ -74,6 +74,6 @@ export const {
       }
       return true;
     },
-    // ...authConfig.callbacks,
+    ...authConfig.callbacks,
   },
 });
